768eb74e6283a7076e3450b45ceb8bf0636ba8f5
1jiwoo27 <chloey1227@naver.com>
2022-11-06 02:52:24 +0900

    edit writing

---
+++ src/components/board/list/ShareBoard.js
@@ -0,0 +1,47 @@
+import React from "react";
+import BoardList from "../BoardList";
+import "../../../css/Board.css";
+import main_search_icon from "../../../images/main-search-icon.png";
+import board_write_icon from "../../../images/board-write-icon.png";
+import { useNavigate } from "react-router-dom";
+ 
+function ShareBoard() {
+
+  const navigate = useNavigate();
+ 
+  const navigateToWriting = () => {
+    navigate("/shareboard/writing");
+  };
+
+  return (
+    <>
+      <div className="board">
+        <div className="board-top">
+          <div className="board-search">
+            <input
+              className="board-search-input"
+              type="text"
+              placeholder="검색어를 입력하세요"
+            />
+            <img className="board-search-icon" src={main_search_icon} />
+          </div>
+          
+          <button className="board-write-button" onClick={navigateToWriting}>
+            <img className="board-write-icon" src={board_write_icon} />
+            <div className="board-write-text">글쓰기</div>
+          </button>
+          
+        </div>
+    
+        <div className="board-middle">
+            <BoardList/>
+        </div>
+
+        <div className="board-bottom">
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ShareBoard;
