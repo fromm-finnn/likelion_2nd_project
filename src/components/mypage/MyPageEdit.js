d174fd98e9e6b57c05ced89b30903b47f524428d
1jiwoo27 <chloey1227@naver.com>
2022-11-06 03:36:35 +0900

    mypage

---
+++ src/components/mypage/MyPageEdit.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../css/MyPageEdit.css";
+import { useNavigate } from "react-router-dom";
+
+function MyPageEdit() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="my-page-edit"></div>
+    </>
+  );
+}
+
+export default MyPageEdit;
