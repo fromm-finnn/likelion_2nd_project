1cba76d193200e2ce6a53449dd9f3b21676fdef5
fromm-finnn <kdc5036@gmail.com>
2022-11-13 17:12:08 +0900

    edit noticelist.js

--- src/components/notice/NoticeList.js
+++ src/components/notice/NoticeList.js
@@ -3,110 +3,110 @@ import Pagination from "../Pagination";
 import "../../css/NoticeList.css";
 import image_icon from "../../images/image-icon.png";
 
-
 function NoticeList() {
   const noticeList = {
     contents: [
       {
         title: "제목1",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목2",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목3",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목4",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목5",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목6",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목7",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목8",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목9",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목10",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목11",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목12",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목13",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목14",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목15",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목16",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목17",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목18",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
         title: "제목19",
         nickname: "운영자",
-        time: "2022.09.29"
+        time: "2022.09.29",
       },
       {
+        id: "20",
         title: "제목20",
         nickname: "운영자",
-        time: "2022.09.29"
-      }
+        time: "2022.09.29",
+      },
     ],
   };
 
@@ -120,26 +120,44 @@ function NoticeList() {
       <div className="notice">
         <div className="notice-middle">
           <div className="notice-card">
-            {noticeList.contents.slice(offset, offset + limit).map((el, index) => {
-             return (
-              <div className="notice-list-box" key={index}>
-                <p className="notice-list-title">{el.title}</p>
-                <div className="notice-list-bottom">
-                  <p className="notice-list-detail">{el.nickname} {el.time} </p>
-                  <img className="notice-image-icon" src={image_icon} />
-                </div>
-              </div>
-              )
-            })}
+            {noticeList.contents
+              .slice(offset, offset + limit)
+              .map((el, index) => {
+                return (
+                  <div>
+                    {index === 19 ? (
+                      <div className="notice-list-box notice-list-last-box">
+                        <p className="notice-list-title">{el.title}</p>
+                        <div className="notice-list-bottom">
+                          <p className="notice-list-detail">
+                            {el.nickname} {el.time}{" "}
+                          </p>
+                          <img className="notice-image-icon" src={image_icon} />
+                        </div>
+                      </div>
+                    ) : (
+                      <div className="notice-list-box">
+                        <p className="notice-list-title">{el.title}</p>
+                        <div className="notice-list-bottom">
+                          <p className="notice-list-detail">
+                            {el.nickname} {el.time}{" "}
+                          </p>
+                          <img className="notice-image-icon" src={image_icon} />
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
           </div>
           <Pagination
-          total={noticeList.contents.length}
-          limit={limit}
-          page={page}
-          setPage={setPage}
-        />
+            total={noticeList.contents.length}
+            limit={limit}
+            page={page}
+            setPage={setPage}
+          />
         </div>
-        <div className="notice-bottom"/>
+        <div className="notice-bottom" />
       </div>
     </>
   );
